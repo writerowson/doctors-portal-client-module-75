ebcdc4db4525aba34fbbf4603291bd9cc2748de7
witerowson <runurowson@gmail.com>
2022-05-20 17:47:52 +0600

    first commit

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -11,10 +11,11 @@ const UserRow = ({ user, refetch }) => {
             }
         })
             .then(res => {
-                if(res.status === 403){
+                if (res.status === 403) {
                     toast.error('Failed to Make an admin');
                 }
-                return res.json()})
+                return res.json()
+            })
             .then(data => {
                 if (data.modifiedCount > 0) {
                     refetch();
@@ -27,8 +28,8 @@ const UserRow = ({ user, refetch }) => {
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
-            <td><button class="btn btn-xs">Remove User</button></td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
+            <td><button className="btn btn-xs">Remove User</button></td>
         </tr>
     );
 };
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -6,7 +6,7 @@ import UserRow from './UserRow';
 const Users = () => {
     const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
         method: 'GET',
-        headers:{
+        headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
     }).then(res => res.json()));
@@ -16,8 +16,8 @@ const Users = () => {
     return (
         <div>
             <h2 className="text-2xl">All Users: {users.length}</h2>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
+            <div className="overflow-x-auto">
+                <table className="table w-full">
                     <thead>
                         <tr>
                             <th></th>
@@ -27,13 +27,13 @@ const Users = () => {
                         </tr>
                     </thead>
                     <tbody>
-                       {
-                           users.map(user=><UserRow
-                           key={user._id}
-                           user={user}
-                           refetch={refetch}
-                           ></UserRow>)
-                       }
+                        {
+                            users.map(user => <UserRow
+                                key={user._id}
+                                user={user}
+                                refetch={refetch}
+                            ></UserRow>)
+                        }
                     </tbody>
                 </table>
             </div>
